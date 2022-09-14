9dce71e3c6d121f072fc96ea429baf4f59e9380f
Ruslan Nigmatullin <nruslanr95@gmail.com>
2022-09-14 08:32:51 +0400

    Auxiliary Data Set's processing function brought

--- AuxDataSetFunctions/AuxDataSetFunctionsUnit.pas
+++ AuxDataSetFunctions/AuxDataSetFunctionsUnit.pas
@@ -21,7 +21,35 @@ procedure CreateFieldInDataSet(
   const Size: Integer = 0
 );
 
+procedure AddDataSetRecord(
+  DataSet: TDataSet;
+  const FieldNames: array of String;
+  const FieldValues: array of Variant
+);
+
+procedure AddDataSetRecordByIndices(
+  DataSet: TDataSet;
+  const FieldValues: array of Variant
+);
+
+procedure ChangeDataSetRecord(
+  DataSet: TDataSet;
+  const FieldNames: array of String;
+  const FieldValues: array of Variant
+); overload;
+
+procedure ChangeDataSetRecordByFieldIndices(
+  DataSet: TDataSet;
+  FieldValues: array of Variant
+); overload;
+
+function GetDataSetFieldMaxValue(DataSet: TDataSet; const FieldName: String): Variant;
+
 implementation
+
+uses
+
+  Variants;
 
 function IsDataSetAtFirstRecord(ADataSet: TDataSet): Boolean;
 begin
@@ -73,6 +101,183 @@ begin
   FieldDef.Size := Size;
   
   FieldDef.CreateField(DataSet);
+
+end;
+
+procedure SetDataSetFieldValues(
+  DataSet: TDataSet;
+  const FieldNames: array of String;
+  const FieldValues: array of Variant
+);
+var
+    I: Integer;
+begin
+
+  for I := 0 to High(FieldNames) do
+    DataSet.FieldByName(FieldNames[I]).Value := FieldValues[I];
+
+end;
+
+procedure SetDataSetFieldValuesByIndices(
+  DataSet: TDataSet;
+  const FieldValues: array of Variant
+);
+  var
+    I, J: Integer;
+begin
+
+  J := 0;
+
+  for I := DataSet.FieldCount - Length(FieldValues) to DataSet.FieldCount - 1
+  do begin
+
+    DataSet.Fields.Fields[I].Value := FieldValues[J];
+
+    Inc(J);
+
+  end;
+
+end;
+
+procedure AddDataSetRecord(
+  DataSet: TDataSet;
+  const FieldNames: array of String;
+  const FieldValues: array of Variant
+);
+var
+    I: Integer;
+begin
+
+  DataSet.DisableControls;
+
+  try
+
+    DataSet.Append;
+
+    SetDataSetFieldValues(DataSet, FieldNames, FieldValues);
+
+    DataSet.Post;
+
+  finally
+
+    DataSet.EnableControls;
+
+  end;
+
+end;
+
+procedure AddDataSetRecordByIndices(
+  DataSet: TDataSet;
+  const FieldValues: array of Variant
+);
+begin
+
+  DataSet.DisableControls;
+
+  try
+
+    DataSet.Append;
+
+    SetDataSetFieldValuesByIndices(DataSet, FieldValues);
+
+    DataSet.Post;
+
+  finally
+
+    DataSet.EnableControls;
+
+  end;
+
+end;
+
+
+procedure ChangeDataSetRecord(
+  DataSet: TDataSet;
+  const FieldNames: array of String;
+  const FieldValues: array of Variant
+);
+begin
+
+  DataSet.DisableControls;
+
+  try
+
+    DataSet.Edit;
+
+    SetDataSetFieldValues(DataSet, FieldNames, FieldValues);
+
+    DataSet.Post;
+
+  finally
+
+    DataSet.EnableControls;
+
+  end;
+
+end;
+
+procedure ChangeDataSetRecordByFieldIndices(
+  DataSet: TDataSet;
+  FieldValues: array of Variant
+);
+begin
+
+  DataSet.DisableControls;
+
+  try
+
+    DataSet.Edit;
+
+    SetDataSetFieldValuesByIndices(DataSet, FieldValues);
+
+    DataSet.Post;
+
+  finally
+
+    DataSet.EnableControls;
+
+  end;
+
+end;
+
+function GetDataSetFieldMaxValue(DataSet: TDataSet; const FieldName: String): Variant;
+var
+    MaxValue: Variant;
+    CurrentValue: Variant;
+    RecordPointer: Pointer;
+begin
+
+  RecordPointer := DataSet.GetBookmark;
+
+  try
+
+    while not DataSet.Eof do begin
+
+      CurrentValue := DataSet.FieldByName(FieldName).AsVariant;
+
+      if VarIsNull(CurrentValue) or VarIsClear(CurrentValue) then begin
+
+        DataSet.Next;
+
+        Continue;
+
+      end;
+
+      if VarIsClear(MaxValue) or (MaxValue < CurrentValue)
+      then MaxValue := CurrentValue;
+
+      DataSet.Next;
+
+    end;
+
+    Result := MaxValue;
+    
+  finally
+
+    DataSet.GotoBookmark(RecordPointer);
+    DataSet.FreeBookmark(RecordPointer);
+
+  end;
 
 end;
 
