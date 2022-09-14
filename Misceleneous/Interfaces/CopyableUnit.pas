df47c2c17a6afd6011c9348770be7983fbf55dcd
Ruslan Nigmatullin <nruslanr95@gmail.com>
2022-09-14 08:48:48 +0400

    Copyeable Interface's methods are included an additional param specifying whether invariants compilance ensuring is needed during a copy operation

--- Misceleneous/Interfaces/CopyableUnit.pas
+++ Misceleneous/Interfaces/CopyableUnit.pas
@@ -6,11 +6,20 @@ uses SysUtils, IGetSelfUnit;
 
 type
 
+  TInvariantsEnsuringType = (ieInvariantsEnsuringRequested, ieNotInvariantsEnsuring);
+
   ICopyable = interface(IGetSelf)
   ['{1DDD8321-9911-414E-A3A8-41FE25F31E2F}']
-  
-    procedure CopyFrom(Copyable: TObject);
-    procedure DeepCopyFrom(Copyable: TObject);
+
+    procedure CopyFrom(
+      Copyable: TObject;
+      const InvariantsEnsuringType: TInvariantsEnsuringType = ieNotInvariantsEnsuring
+    );
+
+    procedure DeepCopyFrom(
+      Copyable: TObject;
+      const InvariantsEnsuringType: TInvariantsEnsuringType = ieNotInvariantsEnsuring
+    ); 
 
   end;
 
